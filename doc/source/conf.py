9ed2234261ce2994fbb8ae4fea1a83a4b4a27a97
righterTY <54281200+righterTY@users.noreply.github.com>
2022-12-17 18:00:16 +0800

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,6 +35,6 @@ language = 'Python'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('https://github.com/righterTY/Polarization-Defect-Detection'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
