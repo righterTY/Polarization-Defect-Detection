824e954355242a857305b6872a9522a53fcbfe53
righterTY <54281200+righterTY@users.noreply.github.com>
2022-12-17 18:30:27 +0800

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,7 +21,6 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosectionlabel',
-    'myst_parser',
     'sphinx_copybutton',
     'sphinx.ext.autodoc.typehints',
 ]  # yapf: disable
